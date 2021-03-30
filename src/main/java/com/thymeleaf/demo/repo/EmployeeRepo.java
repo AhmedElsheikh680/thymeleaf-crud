de3019d54c1ec220f04c91ff0cdffe259c759f39
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-30 17:54:28 +0200

    Add Method To Sort By LastName Closes #8

--- src/main/java/com/thymeleaf/demo/repo/EmployeeRepo.java
+++ src/main/java/com/thymeleaf/demo/repo/EmployeeRepo.java
@@ -4,6 +4,10 @@ import com.thymeleaf.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+    // Add Method To Sort By LastName
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
