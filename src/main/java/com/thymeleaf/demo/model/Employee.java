7cd7eee2e95f0986b957056b9a74b6340d2305fa
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-30 16:17:56 +0200

    Adding Model(Employee) Closes #2

---
+++ src/main/java/com/thymeleaf/demo/model/Employee.java
@@ -0,0 +1,32 @@
+package com.thymeleaf.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
